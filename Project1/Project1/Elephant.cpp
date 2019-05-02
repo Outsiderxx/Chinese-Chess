dd701d9f0c2b45c6264111ef33bfc0a77fa36331
mouse0218 <49981548+mouse0218@users.noreply.github.com>
2019-05-02 16:15:52 +0800

    新Elephant

--- Project1/Project1/Elephant.cpp
+++ Project1/Project1/Elephant.cpp
@@ -1,8 +1,4 @@
 #include"Elephant.h"
-#include<iostream>
-#include<iomanip>
-using namespace std;
-Elephant::Elephant() {}
 
 Elephant::Elephant(int nowx, int nowy, int type)
 {
@@ -10,20 +6,17 @@ Elephant::Elephant(int nowx, int nowy, int type)
 	sety(nowy);
 	if (type == 3)
 	{
-		camp = false;		//黑
+		setCamp(false);		//黑
 	}
 	else if (type == 10)
 	{
-		camp = true;		//紅
+		setCamp(true);		//紅
 	}
 	moveCount = 0;			//象在垂直移動時計數
 }
 
-Elephant::~Elephant() {}
-
-void Elephant::move(int dest_x, int dest_y)
+bool Elephant::isMovable(int dest_x, int dest_y, vector<vector<int> > board)
 {
-	int board[9][10] = { 0 };
 	int nowx = getx();
 	int nowy = gety();
 	if (dest_x == nowx && dest_y == nowy)
@@ -34,184 +27,107 @@ void Elephant::move(int dest_x, int dest_y)
 	{
 		//重新輸入目的地
 	}
+	if (board[dest_x][dest_y] != 0)				//判斷目標位置是不是友軍
+	{
+		if (board[dest_x][dest_y] <= 7 && !getCamp())
+		{
+			return 0;
+		}
+		if (board[dest_x][dest_y] > 7 && getCamp())
+		{
+			return 0;
+		}
+	}
 	if (dest_x == nowx + 2 && dest_y == nowy + 2)	//右下移動
 	{
-		if (moveCount == 2 && !camp)				//象不過河
+		if (moveCount == 2 && !getCamp())			//象不過河
 		{
-			//you shall not pass
+			return 0;
 		}
-		else if (0/*目標位置有友軍棋子*/)
+		if (board[nowx + 1][nowy + 1] != 0)			//塞象眼
 		{
-			//you shall not pass
-		}
-		else if (board[nowx + 1][nowy + 1] != 0)	//塞象眼
-		{
-			//you shall not pass
+			return 0;
 		}
 		else
 		{
-			board[nowx][nowy] = 0;
-			if (!camp)					//黑方
+			if (!getCamp())							//黑方
 			{
 				moveCount++;
-				board[dest_x][dest_y] = 3;
 			}
-			else						//紅方
+			else									//紅方
 			{
 				moveCount--;
-				board[dest_x][dest_y] = 10;
 			}
-			setx(dest_x);
-			sety(dest_y);
-
-
-
-			for (int loop = 0; loop < 10; loop++)
-			{
-				for (int loop1 = 0; loop1 < 9; loop1++)
-				{
-					cout << setw(3) << board[loop1][loop];
-				}
-				cout << endl;
-			}
-
-
-
+			return 1;
 		}
 	}
 	if (dest_x == nowx - 2 && dest_y == nowy + 2)	//左下移動
 	{
-		if (moveCount == 2 && !camp)				//象不過河
+		if (moveCount == 2 && !getCamp())			//象不過河
 		{
-			//you shall not pass
+			return 0;
 		}
-		else if (0/*目標位置有友軍棋子*/)
+		if (board[nowx - 1][nowy + 1] != 0)			//塞象眼
 		{
-			//you shall not pass
-		}
-		else if (board[nowx - 1][nowy + 1] != 0)	//塞象眼
-		{
-			//you shall not pass
+			return 0;
 		}
 		else
 		{
-			board[nowx][nowy] = 0;
-			if (!camp)					//黑方
+			if (!getCamp())							//黑方
 			{
 				moveCount++;
-				board[dest_x][dest_y] = 3;
 			}
-			else						//紅方
+			else									//紅方
 			{
 				moveCount--;
-				board[dest_x][dest_y] = 10;
 			}
-			setx(dest_x);
-			sety(dest_y);
-
-
-
-			for (int loop = 0; loop < 10; loop++)
-			{
-				for (int loop1 = 0; loop1 < 9; loop1++)
-				{
-					cout << setw(3) << board[loop1][loop];
-				}
-				cout << endl;
-			}
-
-
-
+			return 1;
 		}
 	}
 	if (dest_x == nowx + 2 && dest_y == nowy - 2)	//右上移動
 	{
-		if (moveCount == 2 && camp)					//象不過河
+		if (moveCount == 2 && getCamp())			//象不過河
 		{
-			//you shall not pass
+			return 0;
 		}
-		else if (0/*目標位置有友軍棋子*/)
+		if (board[nowx + 1][nowy - 1] != 0)			//塞象眼
 		{
-			//you shall not pass
-		}
-		else if (board[nowx + 1][nowy - 1] != 0)	//塞象眼
-		{
-			//you shall not pass
+			return 0;
 		}
 		else
 		{
-			board[nowx][nowy] = 0;
-			if (!camp)					//黑方
+			if (!getCamp())							//黑方
 			{
 				moveCount--;
-				board[dest_x][dest_y] = 3;
 			}
-			else						//紅方
+			else									//紅方
 			{
 				moveCount++;
-				board[dest_x][dest_y] = 10;
 			}
-			setx(dest_x);
-			sety(dest_y);
-
-
-
-			for (int loop = 0; loop < 10; loop++)
-			{
-				for (int loop1 = 0; loop1 < 9; loop1++)
-				{
-					cout << setw(3) << board[loop1][loop];
-				}
-				cout << endl;
-			}
-
-
-
+			return 1;
 		}
 	}
 	if (dest_x == nowx - 2 && dest_y == nowy - 2)	//左上移動
 	{
-		if (moveCount == 2 && camp)					//象不過河
+		if (moveCount == 2 && getCamp())			//象不過河
 		{
-			//you shall not pass
+			return 0;
 		}
-		else if (0/*目標位置有友軍棋子*/)
+		if (board[nowx - 1][nowy - 1] != 0)			//塞象眼
 		{
-			//you shall not pass
-		}
-		else if (board[nowx - 1][nowy - 1] != 0)	//塞象眼
-		{
-			//you shall not pass
+			return 0;
 		}
 		else
 		{
-			board[nowx][nowy] = 0;
-			if (!camp)					//黑方
+			if (!getCamp)							//黑方
 			{
 				moveCount--;
-				board[dest_x][dest_y] = 3;
 			}
-			else						//紅方
+			else									//紅方
 			{
 				moveCount++;
-				board[dest_x][dest_y] = 10;
 			}
-			setx(dest_x);
-			sety(dest_y);
-
-
-
-			for (int loop = 0; loop < 10; loop++)
-			{
-				for (int loop1 = 0; loop1 < 9; loop1++)
-				{
-					cout << setw(3) << board[loop1][loop];
-				}
-				cout << endl;
-			}
-
-
-
+			return 1;
 		}
 	}
 }
