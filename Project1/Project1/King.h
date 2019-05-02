afd3383cb004e5262391068b6e13f32eb4840bac
mouse0218 <49981548+mouse0218@users.noreply.github.com>
2019-05-02 21:44:56 +0800

    更新將的走法

--- Project1/Project1/King.h
+++ Project1/Project1/King.h
@@ -3,6 +3,6 @@ class King : public chessBasic
 {
 public:
 	King(int, int, int);
-	bool isMovable(int, int, const int *board[]);
+	bool isMovable(int, int, vector<vector<int> >);
 	bool kingMeetKing(King&, const int *board[]);
 };
