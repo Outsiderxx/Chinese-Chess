ccc9be4e51b53c081a39971cb8899e7e19da76cc
Kecher626 <49981537+Kecher626@users.noreply.github.com>
2019-05-13 13:31:50 +0800

    離開介面完成修正+重播選擇+讀檔q可清除畫面+炮車bug修正(戰況未完成)版

--- star burst stream/star burst stream/Bubble.cpp
+++ star burst stream/star burst stream/Bubble.cpp
@@ -36,17 +36,21 @@ bool bubble::isMovable(int dest_x, int dest_y, vector<vector<int>> board)
 					//move again.
 					return 0;
 				}
-				else
+				else if(obstacleCount == 1)
 				{
-					if (obstacleCount == 1)
+					if (board[dest_y][dest_x] == 0) return 0; //no target to eat.
+					else
 					{
-						if (board[dest_y][dest_x] == 0) return 0; //no target to eat.
-						else
-						{
-							return 1;
-						}
+						return 1;
 					}
-					else return 1;
+				}
+				else if (obstacleCount == 0)
+				{
+					if (board[dest_y][dest_x] != 0) return 0; //no target to eat.
+					else
+					{
+						return 1;
+					}
 				}
 			}
 			else if (dest_y > nowy) //go down.
@@ -62,17 +66,21 @@ bool bubble::isMovable(int dest_x, int dest_y, vector<vector<int>> board)
 				{
 					return 0;
 				}
-				else
+				else if (obstacleCount == 1)
 				{
-					if (obstacleCount == 1)
+					if (board[dest_y][dest_x] == 0) return 0; //no target to eat.
+					else
 					{
-						if (board[dest_y][dest_x] == 0) return 0; //no target to eat.
-						else
-						{
-							return 1;
-						}
+						return 1;
 					}
-					else return 1;
+				}
+				else if (obstacleCount == 0)
+				{
+					if (board[dest_y][dest_x] != 0) return 0; //no target to eat.
+					else
+					{
+						return 1;
+					}
 				}
 			}
 		}
@@ -92,17 +100,21 @@ bool bubble::isMovable(int dest_x, int dest_y, vector<vector<int>> board)
 				{
 					return 0;
 				}
-				else
+				else if (obstacleCount == 1)
 				{
-					if (obstacleCount == 1)
+					if (board[dest_y][dest_x] == 0) return 0; //no target to eat.
+					else
 					{
-						if (board[dest_y][dest_x] == 0) return 0; //no target to eat.
-						else
-						{
-							return 1;
-						}
+						return 1;
 					}
-					else return 1;
+				}
+				else if (obstacleCount == 0)
+				{
+					if (board[dest_y][dest_x] != 0) return 0; //no target to eat.
+					else
+					{
+						return 1;
+					}
 				}
 			}
 			else if (dest_x > nowx) //go right.
@@ -118,17 +130,21 @@ bool bubble::isMovable(int dest_x, int dest_y, vector<vector<int>> board)
 				{
 					return 0;
 				}
-				else
+				else if (obstacleCount == 1)
 				{
-					if (obstacleCount == 1)
+					if (board[dest_y][dest_x] == 0) return 0; //no target to eat.
+					else
 					{
-						if (board[dest_y][dest_x] == 0) return 0; //no target to eat.
-						else
-						{
-							return 1;
-						}
+						return 1;
 					}
-					else return 1;
+				}
+				else if (obstacleCount == 0)
+				{
+					if (board[dest_y][dest_x] != 0) return 0; //no target to eat.
+					else
+					{
+						return 1;
+					}
 				}
 			}
 		}
@@ -143,4 +159,119 @@ bool bubble::isMovable(int dest_x, int dest_y, vector<vector<int>> board)
 		//move again.
 		return 0;
 	}
+}
+
+vector<vector<bool>> bubble::hint(int nowx, int nowy, vector<vector<int>> board)
+{
+	// initial the hint board
+	vector<bool> zeroRows;
+	vector<vector<bool>> hint_board;
+	bool tempBool = false;
+	for (int i = 0; i < 9; ++i)
+	{
+		zeroRows.push_back(tempBool);
+	}
+	for (int i = 0; i < 10; ++i)
+	{
+		hint_board.push_back(zeroRows);
+	}
+	//--------------------------------------
+	int obstacleCnt = 0;
+	for (int i = nowx - 1; i >= 0; --i) // go left
+	{
+		if (board[nowy][i] == 0)
+		{
+			hint_board[nowy][i] = true;
+		}
+		else
+		{
+			obstacleCnt++;
+			if (obstacleCnt == 2)
+			{
+				if (board[nowy][i] >= 1 && board[nowy][i] <= 7)
+				{
+					hint_board[nowy][i] = getCamp() ? true : false;
+				}
+				else if (board[nowy][i] >= 8 && board[nowy][i] <= 14)
+				{
+					hint_board[nowy][i] = getCamp() ? false : true;
+				}
+				obstacleCnt = 0;
+				break;
+			}
+		}
+	}
+	for (int i = nowx + 1; i <= 8; ++i) // go right
+	{
+		if (board[nowy][i] == 0)
+		{
+			hint_board[nowy][i] = true;
+		}
+		else
+		{
+			obstacleCnt++;
+			if (obstacleCnt == 2)
+			{
+				if (board[nowy][i] >= 1 && board[nowy][i] <= 7)
+				{
+					hint_board[nowy][i] = getCamp() ? true : false;
+				}
+				else if (board[nowy][i] >= 8 && board[nowy][i] <= 14)
+				{
+					hint_board[nowy][i] = getCamp() ? false : true;
+				}
+				obstacleCnt = 0;
+				break;
+			}
+		}
+	}
+	for (int i = nowy - 1; i >= 0; --i) // go up
+	{
+		if (board[i][nowx] == 0)
+		{
+			hint_board[i][nowx] = true;
+		}
+		else
+		{
+			obstacleCnt++;
+			if (obstacleCnt == 2)
+			{
+				if (board[i][nowx] >= 1 && board[i][nowx] <= 7)
+				{
+					hint_board[i][nowx] = getCamp() ? true : false;
+				}
+				else if (board[i][nowx] >= 8 && board[i][nowx] <= 14)
+				{
+					hint_board[i][nowx] = getCamp() ? false : true;
+				}
+				obstacleCnt = 0;
+				break;
+			}
+		}
+	}
+	for (int i = nowy + 1; i <= 9; ++i) // go down
+	{
+		if (board[i][nowx] == 0)
+		{
+			hint_board[i][nowx] = true;
+		}
+		else
+		{
+			obstacleCnt++;
+			if (obstacleCnt == 2)
+			{
+				if (board[i][nowx] >= 1 && board[i][nowx] <= 7)
+				{
+					hint_board[i][nowx] = getCamp() ? true : false;
+				}
+				else if (board[i][nowx] >= 8 && board[i][nowx] <= 14)
+				{
+					hint_board[i][nowx] = getCamp() ? false : true;
+				}
+				obstacleCnt = 0;
+				break;
+			}
+		}
+	}
+	return hint_board;
 }
--- star burst stream/star burst stream/Bubble.h
+++ star burst stream/star burst stream/Bubble.h
@@ -5,4 +5,5 @@ class bubble : public chessBasic
 public:
 	bubble(void) {}
 	virtual bool isMovable(int, int, const vector<vector<int>>);
+	virtual vector<vector<bool>> hint(int, int, vector<vector<int>>);
 };
